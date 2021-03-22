1a0400c1f59342a165c5ee8957307d65319c3088
lauri11 <elena.korzhuk@gmail.com>
2021-03-22 18:42:47 +0000

    More practise

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+setInterval(updateClock, 1000);
+
+function updateClock(){
+    const clockContainer = document.querySelector('.clock');
+    clockContainer.innerText = new Date().toLocaleTimeString();    
+}
